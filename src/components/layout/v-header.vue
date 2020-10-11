63ef293f61676af81c2acf5203ad52cb53f4479f
Denisalik <d.shchegletov@innopolis.university>
2020-10-11 14:27:38 +0300

    updated log-in form

--- src/components/layout/v-header.vue
+++ src/components/layout/v-header.vue
@@ -9,16 +9,21 @@
             <b-button pill variant="info" v-b-modal="'log_in_form'" class="log-in-button">Log-in</b-button>
 
         </div>
-        <b-modal size="lg" id='log_in_form' centered title="Log-in" ok-only button-size="lg">
-            <p>enter you login and password</p>
-            <p>Write form here!</p>
+        <b-modal size="lg" id='log_in_form' centered title="Enter your login and password" ok-only button-size="lg" ok-variant="info">
+			<template v-slot:modal-footer="{ ok }">
+				<b-button size="sm" variant="info" @click="ok()" class="log-in-form-button" pill>Log in</b-button>
+			</template>
+            <b-form-input :id="'login'" :name="'log-in-input'" :type="'text'" :placeholder="'Login'" size="sm" required class="input-search"></b-form-input>
+			<b-form-input :id="'password'" :name="'log-in-input'" :type="'password'" :placeholder="'Password'" size="sm" required class="input-search"></b-form-input>
+			<b-form-checkbox :id="'remember'" :name="'log-in-input'" v-model="selected">Remember me</b-form-checkbox>
         </b-modal>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'v-header'
+        name: 'v-header',
+		methods: {}
     }
 </script>
 
