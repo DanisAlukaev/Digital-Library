6f8345d7d96becf2c9a40b3dbdec7e4d87465bbd
tasneem22 <tasneemsherif23@gmail.com>
2020-11-08 19:52:07 +0300

    uploading

---
+++ Desktop/Digital-Library-master3/Digital-Library-master/backend/Comments part/Storage/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a new tag")
+
+    def __str__(self):
+        return self.name
+
+
+class Upload(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    document = models.FileField(upload_to='documents/')
+    tags = models.ManyToManyField(Tag, help_text="Select the tags")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('Content-detail', kwargs={'pk': self.pk})
