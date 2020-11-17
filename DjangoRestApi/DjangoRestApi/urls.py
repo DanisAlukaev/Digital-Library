93ba69a134e163348ad03e62a92fdff6dbdf3736
Danis Alukaev <d.alukaev@innopolis.university>
2020-11-17 08:04:33 +0300

    + dummy upload

--- DjangoRestApi/DjangoRestApi/urls.py
+++ DjangoRestApi/DjangoRestApi/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
+    # Include URL patterns from storage app
+    url('api/', include('storage.urls')),
 ]
